aa2ce3acf41bb9f0fbf762908b64a9d531b01857
Klaas <klaas.degkwitz@baqend.com>
2022-05-01 19:37:58 +0200

    first level start

---
+++ 2dPlatformer/Assets/move.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Vector3 startPosition = Vector3.zero;
+    public float range = 6;
+    bool back = false;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if(transform.position.x < startPosition.x + range && !back)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                                    new Vector3(transform.position.x + 1,
+                                                transform.position.y,
+                                                transform.position.z),
+                                    1 * Time.deltaTime);
+        }
+        else if (transform.position.x > startPosition.x + range || back)
+        {
+            back = true;
+            transform.position = Vector3.MoveTowards(transform.position,
+                                    new Vector3(startPosition.x - 1,
+                                                transform.position.y,
+                                                transform.position.z),
+                                    1 * Time.deltaTime);
+        }
+        if(transform.position.x < startPosition.x)
+        {
+            back = false;
+        }
+    }
+}
