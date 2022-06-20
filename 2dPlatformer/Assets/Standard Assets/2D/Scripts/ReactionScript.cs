e9f750371afdb776f8a59a9c78dd390aec623ca1
Klaas <kl3sk@gmx.net>
2022-06-20 14:41:57 +0200

    changing interaction reactions to fit story

--- 2dPlatformer/Assets/Standard Assets/2D/Scripts/ReactionScript.cs
+++ 2dPlatformer/Assets/Standard Assets/2D/Scripts/ReactionScript.cs
@@ -21,27 +21,32 @@ public class ReactionScript : MonoBehaviour
 			if(reacting_to == "talked"){
 				Debug.Log("reacting to talking");
 				if(gameObject.name=="TalkingBat"){
-					enableTextScriptWithIndexForCharacter(frog,2);
+					enableTextScriptWithIndexForCharacter(frog,1);
+				}
+				if(gameObject.name=="MaskGuy"){
+					enableTextScriptWithIndexForCharacter(frog,3);
 				}
 			}
 			if(reacting_to == "correct"){
-				Debug.Log("reacting to correct answer");
-				Debug.Log(gameObject.name);
-				if(gameObject.name=="MaskGuy"){
-					BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
-					boxCollider.enabled=false;
-				}
-				if(gameObject.name=="NinjaFrog"){
-					Debug.Log("no action");
-					enableTextScriptWithIndexForCharacter(frog,1);
-				}
+				Debug.Log("no action");
 			}
 			else if (reacting_to == "wrong"){
-				Debug.Log("reacting to incorrect");
-				Debug.Log(gameObject.name);
-				if(gameObject.name=="MaskGuy"){
-					Debug.Log("no action");
+			}
+		}
+
+
+
+		if(reaction_index == 1){
+			if(reacting_to == "talked"){
+				Debug.Log("no action");
+			}
+			if(reacting_to == "correct"){
+				if(gameObject.name=="NinjaFrog"){
+					enableTextScriptWithIndexForCharacter(frog,2);
 				}
+
+			}
+			else if (reacting_to == "wrong"){
 				if(gameObject.name=="NinjaFrog"){
 					GameObject FrogTeleportTarget = GameObject.Find("FrogTeleportTarget");
 					if(FrogTeleportTarget){
@@ -53,19 +58,24 @@ public class ReactionScript : MonoBehaviour
 				}
 			}
 		}
-		if(reaction_index == 1){
+
+
+
+		if(reaction_index == 2){
 			if(reacting_to == "talked"){
 				Debug.Log("no action");
 			}
 			if(reacting_to == "correct"){
 				Debug.Log("no action");
-
 			}
 			else if (reacting_to == "wrong"){
 				Debug.Log("no action");
 			}
 		}
-		if(reaction_index == 2){
+
+
+
+		if(reaction_index == 3){
 			if(reacting_to == "talked"){
 				Debug.Log("no action");
 			}
@@ -77,9 +87,9 @@ public class ReactionScript : MonoBehaviour
 			}
 			else if (reacting_to == "wrong"){
 				if(gameObject.name=="NinjaFrog"){
-					GameObject FrogTeleportTarget2 = GameObject.Find("FrogTeleportTarget2");
-					if(FrogTeleportTarget2){
-						player.transform.position = FrogTeleportTarget2.transform.position;
+					GameObject FrogTeleportTarget = GameObject.Find("FrogTeleportTarget");
+					if(FrogTeleportTarget){
+						player.transform.position = FrogTeleportTarget.transform.position;
 					}
 					else{
 						Debug.Log("teleport target not found");
